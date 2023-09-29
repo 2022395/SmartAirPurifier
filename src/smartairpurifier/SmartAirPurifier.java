bdf5b8d03741ee71eb710a61f175a203fb75ed83
2022395 <2022395@student.cct.ie>
2023-09-29 13:17:00 +0100

    Creation of switch
    Creation of switch and improvement on the menu selection.

--- src/smartairpurifier/SmartAirPurifier.java
+++ src/smartairpurifier/SmartAirPurifier.java
@@ -4,6 +4,8 @@
  */
 package smartairpurifier;
 
+import java.util.Scanner;
+
 /**
  *
  * @author volkan
@@ -14,7 +16,48 @@ public class SmartAirPurifier {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        
+        double oxygenClarity= 70.5;
+        double carbondioxideLevel=10.2;
+        int fanSpeed;
+        int selection= menuSelection();
+        
+        
+        switch (selection){
+            case 1 : {
+                System.out.println("Oxygen Clarity: "+ oxygenClarity);
+                System.out.println("Carbondioxide Level:"+ carbondioxideLevel);
+                break;
+            }
+             case 2 : {
+                AirConditionSystem airConditionSystem = new AirConditionSystem();
+                System.out.println("Living Room Temperature is :"+ airConditionSystem.getLivingRoomTemp());
+                System.out.println("Bedroom Temperature is :"+ airConditionSystem.getBedRoomTemp());
+                System.out.println("Main Hall Temperature is :"+ airConditionSystem.getMainHallTemp());
+                System.out.println("Kitchen Temperature is :"+airConditionSystem.getKitchenTemp());
+                break;
+                
+            }
+            
+            
+        
+            
+        }
     
+        
+        
+        
+    }
+    public static int menuSelection(){
+        System.out.println("Welcome To The SmartAirPurifier Main Menu");
+        System.out.println("Please make a Selection from this menu");
+        System.out.println("1: AirPurifier");
+        System.out.println("2: AirCondition System");
+        Scanner scanner=new Scanner(System.in);
+     
+        String selection = scanner.nextLine();
+        return Integer.parseInt(selection);
+        
+    }
+
 }
