c8d4ea46d107210791eeae087796822cb6bbb86b
2022395 <2022395@student.cct.ie>
2023-09-29 13:34:20 +0100

    New Class (Settings) has been Created
    Update() and ScreenSettings() functions have been added initially.
    More improvements will be added shortly.

---
+++ src/smartairpurifier/Settings.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package smartairpurifier;
+
+/**
+ *
+ * @author volkan
+ */
+public class Settings {
+    
+    public void Update(){
+        System.out.println("System has been updated to version 12.1.45");
+    
+    }
+    public void ScreenSettings(){
+        System.out.println("Screen size 10.1");
+    
+    }
+    
+}
